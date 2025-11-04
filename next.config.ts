826e2c3f666617ec09c01befe9028498539c95ce
Ikbar1 <asyamikbar@gmail.com>
2025-11-04 21:46:59 +0700

    Setup deploy ke GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isGhPages = Boolean(process.env.NEXT_PUBLIC_BASE_PATH?.length)
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'export', // penting: biar hasil build jadi static HTML
+  images: { unoptimized: true }, // biar image jalan di GitHub Pages
+  basePath: isGhPages ? process.env.NEXT_PUBLIC_BASE_PATH : undefined,
+  assetPrefix: isGhPages ? process.env.NEXT_PUBLIC_BASE_PATH + '/' : undefined,
+}
 
-export default nextConfig;
+export default nextConfig
